f146d8d5a31a6df4fda1fbd496994846822034ec
Steven Isbell <steveni_92@msn.com>
2019-01-27 21:28:39 -0700

    edits

--- background.js
+++ background.js
@@ -1,7 +1,1 @@
-chrome.runtime.onMessage.addListener((msg, sender, res) => {
-  if (msg.from === 'popup' && msg.subject === 'courseId') {
-    if (document.getElementById('group-groupId')) {
-      res(document.getElementById('group-groupId').value);
-    }
-  }
-});
+chrome.runtime.onInstalled.addListener(function() {});

299c0628966e40f511a38e38c8da3ee35b3f5e97
Steven Isbell <steveni_92@msn.com>
2019-01-28 22:13:45 -0700

    edits

--- popup.js
+++ popup.js
@@ -1,10 +1,17 @@
 const btn = document.getElementById('click');
+
 btn.addEventListener('click', () => {
   chrome.tabs.query(
     { active: true, lastFocusedWindow: true, currentWindow: true },
     function(tabs) {
-      var url = tabs[0].url;
-      alert(url);
+      const url = tabs[0].url;
+      let resources;
+      chrome.storage.sync.set({ foo: 'hello', bar: 'hi' }, () => {
+        alert('Settings saved');
+      });
+      chrome.storage.sync.get(['foo'], items => {
+        alert('Settings retrieved', items);
+      });
     }
   );
 });
